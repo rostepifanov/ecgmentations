ba421861d96300a02b128c8075ab6aaced021638
Rostislav Epifanov <rostepifanov@gmail.com>
2023-10-05 12:25:32 +0700

    move specific time test to submodule, add mask shape checking for ecgonlytransforms

--- tests/ecgmentations/augmentations/test_transforms.py
+++ tests/ecgmentations/augmentations/test_transforms.py
@@ -4,7 +4,6 @@ import numpy as np
 import ecgmentations as E
 
 SHAPE_PRESERVED_TRANSFORMS = [
-    E.TimeReverse,
     E.AmplitudeInvert,
     E.ChannelShuffle,
     E.ChannelDropout,
@@ -12,8 +11,9 @@ SHAPE_PRESERVED_TRANSFORMS = [
     E.Blur,
     E.GaussBlur,
     E.AmplitudeScale,
-    E.TimeCutout,
+    E.TimeReverse,
     E.RandomTimeWrap,
+    E.TimeCutout,
 ]
 
 SHAPE_UNPRESERVED_TRANSFORMS = [
@@ -48,35 +48,5 @@ def test_Transform_CASE_call(transform):
 
     assert tmask.shape == mask.shape
 
-def test_TimeCrop_CASE_left_crop():
-    ecg = np.random.uniform(size=(12, 5000)).T
-    mask = np.zeros((1, 5000)).T
-
-    expected_length = 4000
-
-    transform = E.TimeCrop(length=expected_length, position='left', always_apply=True)
-    transformed = transform(ecg=ecg, mask=mask)
-
-    tecg, tmask = transformed['ecg'], transformed['mask']
-
-    assert tecg.shape == (expected_length, *ecg.shape[1:])
-    assert tmask.shape == (expected_length, *mask.shape[1:])
-
-    assert pytest.approx(tecg) == ecg[-expected_length:]
-
-def test_TimePadIfNeeded_CASE_left_padding():
-    ecg = np.random.uniform(size=(12, 4000)).T
-    mask = np.zeros((1, 4000)).T
-
-    expected_length = 5000
-
-    transform = E.TimePadIfNeeded(min_length=expected_length, position='left', always_apply=True)
-    transformed = transform(ecg=ecg, mask=mask)
-
-    tecg, tmask = transformed['ecg'], transformed['mask']
-
-    assert tecg.shape == (expected_length, *ecg.shape[1:])
-    assert tmask.shape == (expected_length, *mask.shape[1:])
-
-    assert pytest.approx(tecg[:ecg.shape[0]]) == ecg
-    assert pytest.approx(tecg[ecg.shape[0]:]) == 0.
+    if isinstance(transform, E.EcgOnlyTransform):
+        assert np.all(tmask == mask)
