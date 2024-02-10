4929daabdae4382c1b381a247a1ac35dd05351fa
Rostislav Epifanov <rostepifanov@gmail.com>
2024-02-10 23:20:38 +0700

    add tochannels modification

--- ecgmentations/core/__init__.py
+++ ecgmentations/core/__init__.py
@@ -1,2 +1,3 @@
 from ecgmentations.core.transforms import EcgOnlyTransform, Identity, DualTransform
 from ecgmentations.core.compositions import Sequential, OneOf
+from ecgmentations.core.modifications import ToChannels
